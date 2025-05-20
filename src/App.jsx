b7c960794ba393dc5289a373053a84174c30119f
rohitranjanrax <2022501361.rohit@ug.sharda.ac.in>
2025-05-20 19:41:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    
+    <div>
+      <h1>Welcome to the Job Board!</h1>
+      <p>This is the beginning of your React journey.</p>
+    </div>
+  );
+
+}
+
+export default App
+
+
---
+++ src/Navbar.jsx
@@ -0,0 +1,8 @@
+function Navbar(){
+    return(
+        <h1>
+            Job Board
+        </h1>
+    )
+}
+export default Navbar
