744cbfee1c701ebf36454866300ff96706f885b4
rohitranjanrax <2022501361.rohit@ug.sharda.ac.in>
2025-05-23 18:36:27 +0530

    added  new file

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  HomeLayout,
+  Landing,
+  Register,
+  Login,
+  DashboardLayout,
+  Error,
+  AddJob,
+  Stats,
+  AllJobs,
+  Profile,
+  Admin,
+} from './pages';
 
-function App() {
- 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      { index: true, element: <Landing /> },
+      { path: 'register', element: <Register /> },
+      { path: 'login', element: <Login /> },
+      {
+        path: 'dashboard',
+        element: <DashboardLayout />,
+        children: [
+          { index: true, element: <AddJob /> },
+          { path: 'stats', element: <Stats /> },
+          { path: 'all-jobs', element: <AllJobs /> },
+          { path: 'profile', element: <Profile /> },
+          { path: 'admin', element: <Admin /> },
+        ],
+      },
+    ],
+  },
+]);
 
-  return (
-    
-    <div>
-      <h1>Welcome to the Job Board!</h1>
-      <p>This is the beginning of your React journey.</p>
-    </div>
-  );
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
-}
-
-export default App
-
-
+export default App;
--- src/Navbar.jsx
+++
@@ -1,8 +0,0 @@
-function Navbar(){
-    return(
-        <h1>
-            Job Board
-        </h1>
-    )
-}
-export default Navbar
